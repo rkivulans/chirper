a7756bae16f1d5b2040e98bec9173895474c7076
Kristaps Enkuzens <kristaps@kecom.lv>
2024-07-22 15:04:03 +0300

    Added tabula as example view

---
+++ resources/views/tabula.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 prose">
+        <h1>Šeit būs mana tabula</h1>
+        <p>Ar precēm, un cenām</p>
+        <table>
+        </table>
+    </div>
+</x-app-layout>
+
