603feb6abcab94f543905e44ba97119b93c0bbd9
Rihards Kivulans <ext.rihards@kecom.lv>
2024-07-26 16:38:42 +0300

    Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -14,5 +15,12 @@ class Product extends Model
         'description',
         'price',
         'quantity',
+        'user_id',
+
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
