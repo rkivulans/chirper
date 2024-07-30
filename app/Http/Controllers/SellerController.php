c670cbe0b6de55b7d5fe1481f655965ca319f7f3
Rihards Kivulans <ext.rihards@kecom.lv>
2024-07-30 14:43:03 +0300

    Izveidoju SellerController

---
+++ app/Http/Controllers/SellerController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class SellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $sellers = User::has('products')->get();
+
+        return view('sellers.index', compact('sellers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $seller): View
+    {
+        $products = $seller->products;
+
+        return view('sellers.show', compact('seller', 'products'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Seller $seller)
+    {
+        //
+    }
+}
