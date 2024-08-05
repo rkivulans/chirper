88d841dda1cecbe14557028206556d92ebe4eeb0
Kristaps Enkuzens <kristaps@kecom.lv>
2024-08-05 11:13:28 +0000

    constrained table

--- database/migrations/2024_08_02_111704_update.php
+++ database/migrations/2024_08_02_111704_update.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('product_id')->nullable();
+            $table->foreignId('product_id')->constrained()->nullable();
         });
     }
 
