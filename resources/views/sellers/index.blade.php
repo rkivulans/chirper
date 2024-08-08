a0a7bf9f3318eefd087ba8cb806f11e25393bbf5
Rihards Kivulans <ext.rihards@kecom.lv>
2024-08-08 12:56:19 +0300

    WIP izņēmu lieko komandu zem seller->name.

--- resources/views/sellers/index.blade.php
+++ resources/views/sellers/index.blade.php
@@ -34,11 +34,6 @@
                                     <tr>
                                         <td class="py-4 px-3 text-sm font-medium text-gray-900">
                                             <a href="{{ route('sellers.show', $seller) }}" class="text-indigo-600 hover:text-indigo-900">{{ $seller->name }}</a>
-                                            @if ($seller->favoriteProduct)
-                                                {{ $seller->favoriteProduct->name }}
-                                            @else
-                                                Nav mīļākais prod
-                                            @endif
                                         </td>
                                         <td class="py-4 px-3 text-sm text-gray-500">
                                             @if ($seller->favoriteProduct)
